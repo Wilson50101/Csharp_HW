c24278ea26afd1312b9240f4b67aecd85994a93d
wilson su <wilson40219@hotmail.com>
2019-12-26 11:44:16 +0800

    新增資工系館之經緯度

--- Final Project/NCKU-Eat/NCKU-Eat/Menu.cs
+++ Final Project/NCKU-Eat/NCKU-Eat/Menu.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace NCKU_Eat
 {
     public partial class Menu : Form
     {
+        //資工系館的經緯度
+        PointF PositionCSIE = new PointF(22.996992f, 120.221510f);
+        //建立RetaurantEntities物件,此物件可以管理Restaurant.mdf資料庫
+        RestaurantEntities re = new RestaurantEntities();
         public Menu()
         {
             InitializeComponent();
